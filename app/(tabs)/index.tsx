a4b1b2ac7ecd401302925c29515bd46b12655709
utari <indahwahyuniutari@gmail.com>
2024-11-26 00:03:46 +0700

    push berikutnya

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fcfc03',
+    backgroundColor: '#D2B48C',
   },
   text: {
     fontSize: 30,
